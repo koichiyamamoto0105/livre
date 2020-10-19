49f50771cfaf01016c93b584caa680aeeb5ad8f3
KoichiYamamoto <ko1y5osaka@gmail.com>
2020-10-19 14:43:25 +0000

    []finished 1st day

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+
+  def create
+     book = Book.new(book_params)
+
+    if book.save
+    redirect_to book_path(book.id), notice:'You have creatad book successfully.'
+    else
+      @book = book
+      @books = Book.all
+    render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
