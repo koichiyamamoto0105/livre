016655ea5bc9e42114415d90e06c2b9499f88ea0
KoichiYamamoto <ko1y5osaka@gmail.com>
2020-10-20 11:58:59 +0000

    [Finished 2nd day]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+ 
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      redirect_to books_path, notice:'Book was successfully destroyed.'
+    else
+      render :index
+    end
+  end
+
+
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#home'
   root 'books#index'
   devise_for :users
   resources :books
+  resources :users, only: [:show, :edit]
 end
